01e714373ff439d4354fb4001b54e71779a9ffb5
Juan Fox <foxj0875@gmail.com>
2018-05-22 12:42:41 -0400

    created function for validating that register name is between 4 and 24 characters

---
+++ validation/register.js
@@ -0,0 +1,13 @@
+const validator = require('validator');
+
+module.exports = {
+  registerInputValidat(data) {
+    let errors = {};
+
+    if (!validator.isLength(data.name, {min: 4, max: 24})) {
+      errors.name = 'Name must be between 4 and 24 characters';
+    }
+
+    return errors;
+  }
+}
