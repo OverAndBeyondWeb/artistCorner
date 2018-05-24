913769c29ff5b747d6da5547fef978b8a3fa787c
Juan Fox <foxj0875@gmail.com>
2018-05-24 14:13:44 -0400

    coded basic format for models/Post.js

---
+++ models/Post.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  subject: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String
+  },
+  attachment: {
+    type: String
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
