6b273c20999aad215b4334d637e3067eb66a1c1f
Juan Fox <foxj0875@gmail.com>
2018-05-27 00:17:39 -0400

    react-router-dom Link components are wrapping semantic-ui menu components and working properly

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
+import { Container, Menu, Button } from 'semantic-ui-react'
 
 export default class Navbar extends Component {
 
   render() {
     return (
-      <Menu>
-        <Menu>
-        <Menu.Item header>Our Company</Menu.Item>
-        <Menu.Item name='aboutUs' onClick={this.handleItemClick} />
-        <Menu.Item name='jobs' onClick={this.handleItemClick} />
-        <Menu.Item name='locations' onClick={this.handleItemClick} />
-      </Menu>
-      </Menu>
+      
+        <Menu inverted size='large'>
+          <Container>
+            <Link to=""><Menu.Item as='a'>Home</Menu.Item></Link>
+            <Link to=""><Menu.Item as='a'>Company</Menu.Item></Link>
+            <Link to=""><Menu.Item as='a'>Careers</Menu.Item></Link>
+            <Menu.Item position='right'>
+              <Link to="/login"><Button as='a' inverted>Log in</Button></Link>
+              <Link to="/register"><Button as='a' inverted primary style={{ marginLeft: '0.5em' }}>Sign Up</Button></Link>
+            </Menu.Item>
+          </Container>
+        </Menu>
     )
   }
 }
